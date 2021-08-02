5f50a8e5e794bba630decd52c3a68b2be2c27efa
Quentin Hewitt <quentin.l.hewitt@gmail.com>
2021-08-02 10:13:25 -0500

    wip

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class ConsoleExercises {
+
+    public static void main(String[] args){
+//        double pi = 3.14159;
+//
+//        System.out.format("The value of pi is approximately "+ pi);
+
+        Scanner scanner = new Scanner(System.in);
+    //Question 1
+//        System.out.print("Enter a number: ");
+//        int userInput = scanner.nextInt();
+//
+//        System.out.println("You entered: --> \"" + userInput + "\" <--");
+
+    //Question 2
+//        System.out.print("Enter three words: ");
+//        String userInput = scanner.next();
+//        String userInput1 = scanner.next();
+//        String userInput2 = scanner.next();
+//        System.out.println("You entered: --> \"" + userInput + "\" <--");
+//        System.out.println("You entered: --> \"" + userInput1 + "\" <--");
+//        System.out.println("You entered: --> \"" + userInput2 + "\" <--");
+
+    //Question 3,4
+        //next() only gets one word
+        System.out.print("Enter a sentence: ");
+        String userInput = scanner.nextLine();
+        System.out.println("You entered: --> \"" + userInput + "\" <--");
+    }
+}
--- src/HelloPolaris.java
+++ src/HelloPolaris.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
 public class HelloPolaris {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world");
+//        System.out.println("Hello, world");
+//
+//        int myFavoriteNumber = 27;
+//        System.out.print(myFavoriteNumber);
+//
+//        String myString = "3.14159";
+//        System.out.print(myString);
+
+//        String greeting = "Salutations";
+//        String name = "codeup";
+//        System.out.printf("%s, %s!", greeting, name);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter something: ");
+        String userInput = scanner.next();
+
+        System.out.println("You entered: --> \"" + userInput + "\" <--");
     }
+
 }
