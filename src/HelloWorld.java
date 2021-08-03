e491ae828ef8ab14867ec61a9e33b5464de64d92
Quentin Hewitt <quentin.l.hewitt@gmail.com>
2021-08-03 12:27:27 -0500

    wip

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,24 +2,62 @@ import java.util.Scanner;
 public class HelloWorld {
 
     public static void main(String[] args) {
-//        System.out.println("Hello, world");
-//
+        System.out.println("Hello, world");
+
 //        int myFavoriteNumber = 27;
 //        System.out.print(myFavoriteNumber);
 //
 //        String myString = "3.14159";
 //        System.out.print(myString);
+//
+//        long myNumber = 123L;
+//        float myNumber1 = 3.14F;
 
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int three = (int) "three";
+
+//        int x = 4;
+//        x = x + 5;
+//        System.out.println(x);
+//
+//        int x = 3;
+//        int y = 4;
+////        y = y * x;
+//        y=y*=x;
+//        System.out.println(y);
+//
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x;
+//        x=x/=y;
+//        y=y-=x;
+
+
+
+//        System.out.println(x);
+//        System.out.println(y);
 //        String greeting = "Salutations";
 //        String name = "codeup";
 //        System.out.printf("%s, %s!", greeting, name);
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter something: ");
-        String userInput = scanner.next();
-
-        System.out.println("You entered: --> \"" + userInput + "\" <--");
+//        System.out.print("Enter something: ");
+//        String userInput = scanner.next();
+//
+//        System.out.println("You entered: --> \"" + userInput + "\" <--");
     }
 
 }
