6097ac2404091fc888fa685cf61c294bdd5d77b6
Quentin Hewitt <quentin.l.hewitt@gmail.com>
2021-08-03 10:03:34 -0500

    wip

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -10,7 +10,7 @@ public class ControlFlowExercises {
 //        }
 //
 //  Question B
-//    int i = 2;
+//    int i = 0;
 //        do {
 //            i += 2;
 //            System.out.println(i);
@@ -20,7 +20,7 @@ public class ControlFlowExercises {
      //Question B Part 2
 //        int i = 100;
 //        do {
-//            i += -5;
+//            i -= -5;
 //            System.out.println(i);
 //
 //        }while (i >= -10);
@@ -45,12 +45,66 @@ public class ControlFlowExercises {
 //            System.out.println(i);
 //        }
 
-        for (int i = 0; i <= 100 ; i++) {
+//        for (int i = 0; i <= 100 ; i++) {
+//            if(i%3 == 0 && i%5 == 0) {
+//                System.out.println("FizzBuzz " + i);
+//            } else if(i%5 == 0) {
+//                System.out.println("Buzz " + i);
+//            } else if(i%3 == 0) {
+//                System.out.println("Fizz " + i);
+//            }
+//        }
 
-            if(i%3 == 0) {
-                System.out.println("This is Fizz " + i);
+        Scanner scanner = new Scanner(System.in);
+        boolean userContinues = true;
+
+//        do{
+//            System.out.println("What number would you like to go up to?");
+//            int userInt = scanner.nextInt();
+//            System.out.println("\nHere's your table!\n");
+//            System.out.println("number | squared | cubed");
+//            System.out.println("------ | ------- | ------");
+//            for (int i = 1; i <= userInt ; i++) {
+//                System.out.printf("%-6d", i);
+//                System.out.print(" | ");
+//                System.out.printf("%-7d", i * i);
+//                System.out.print(" | ");
+//                System.out.print(i * i * i);
+//                System.out.println();
+//            }
+//            System.out.println("Would you like to enter another number (y/n)?");
+//            String userResponse = scanner.next();
+//            if (!userResponse.equalsIgnoreCase("y")){
+//                userContinues = false;
+//            }
+
+
+//        }while(userContinues);
+
+        boolean anotherGrade = true;
+
+        do {
+            System.out.println("Please enter numerical grade from  0 - 100");
+            int userGrade = scanner.nextInt();
+            if(userGrade >= 88) {
+                System.out.println("A");
+            } else if(userGrade >= 80) {
+                System.out.println("B");
+            }else if (userGrade >= 67) {
+                System.out.println("C");
+            }else if (userGrade >= 60) {
+                System.out.println("D");
+            }else {
+                System.out.println("F");
             }
-        }
+
+            System.out.println("Do you want to enter a new grade? (y/n) ");
+            String userResponse = scanner.next();
+
+            if(!userResponse.equalsIgnoreCase("y")){
+                anotherGrade = false;
+            }
+        }while(anotherGrade);
 
     }
 }
