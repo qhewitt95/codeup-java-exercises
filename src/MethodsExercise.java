26ea6320674ac65d0142f4c09fb0343a922df17a
Quentin Hewitt <quentin.l.hewitt@gmail.com>
2021-08-04 09:48:29 -0500

    finished making functions

--- src/MethodsExercise.java
+++ src/MethodsExercise.java
@@ -1,2 +1,24 @@
 public class MethodsExercise {
+
+    public static double Addition(double numOne, double numTwo){
+        return numOne + numTwo;
+    }
+    public static double Subtraction(double numOne, double numTwo){
+        return numOne - numTwo;
+    }
+    public static double Multiplication(double numOne, double numTwo){
+        return numOne * numTwo;
+    }
+    public static double Division(double numOne, double numTwo){
+        return numOne / numTwo;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(Addition(5,2));
+        System.out.println(Subtraction(10,5));
+        System.out.println(Multiplication(4,6));
+        System.out.println(Division(20,5));
+    }
+
 }
