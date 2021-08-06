2444e549ae0bfb8045aa992b00d20d8cb3e59498
Quentin Hewitt <quentin.l.hewitt@gmail.com>
2021-08-06 10:11:09 -0500

    added persons

---
+++ src/Person.java
@@ -0,0 +1,30 @@
+public class Person {
+
+private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName(){
+    //TODO: return the person's name
+        return name;
+    }
+
+    public void setName(String name){
+    //TODO: change the name property to the passed value
+        this.name = name;
+    }
+    public void sayHello(){
+    //TODO: print a message to the console using the person's name
+        System.out.println("Hello, " + name);
+    }
+
+    public static void main(String[] args) {
+        Person quentin = new Person("Quentin");
+
+
+
+    }
+}
+
