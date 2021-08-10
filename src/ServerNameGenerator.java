674f1fd811fe553572ef04522fc0f8d6a77ba036
Quentin Hewitt <quentin.l.hewitt@gmail.com>
2021-08-10 14:48:48 -0500

    wip

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,6 +1,20 @@
+import java.util.Random;
+
 public class ServerNameGenerator {
 
+
+        public static String[] adjectives ={"small","hot", "cold", "silly","yell","big","chill", "whimsical", "wet","stinky"};
+        public static String[] nouns = {"ikea kitchen set","ikea table","ikea marble counter top","ball","42inch television"," 400lb bear","20lb monkey","dry tongue","dryscalp","black smoke"};
+
     public static void main(String[] args) {
+        System.out.println("here is your server name");
+        System.out.println(getWord(adjectives) + "-" + getWord(nouns));
+    }
+        public static String getWord(String[] choices) {
+            Random random = new Random();
+            int randomNumber = random.nextInt(choices.length);
+            return choices[randomNumber];
+        }
 
     }
-}
+
